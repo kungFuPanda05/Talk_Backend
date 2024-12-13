83c7ce8bd63e6fd32147b58d78734e38d6fce3ae
The Conqueror <vishal7920821202@gmail.com>
2024-12-13 22:00:37 +0530

    feat

---
+++ src/api/user/getProfile.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import jwtStrategy from '../../strategy/auth/jwtauth';
+import db from '../../../models';
+import { Op } from 'sequelize';
+
+let controller = async (req, res, next) => {
+    let profile = await db.User.findOne({
+        attributes: ['id', 'name', 'email'],
+        where: {
+            id: req.user.id
+        },
+        raw: true
+    })
+    let friendsCount = await db.Friend_Request.count({
+        where: {
+            status: "accepted",
+            [Op.or]: [
+                { from: req.user.id },
+                { to: req.user.id }
+            ]
+        },
+        distinct: true,
+        col: 'id'
+    });
+    profile.friendsCount = friendsCount;
+    try {
+        res.status(200).json({
+            success: true,
+            profile
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const apiRouter = express.Router();
+apiRouter.route('/').get(jwtStrategy, controller);
+export default apiRouter;
+
