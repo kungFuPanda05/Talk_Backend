e75ba1d6b95a3a985578a7af4551148c1a26fad1
The Conqueror <vishal7920821202@gmail.com>
2025-01-02 22:32:36 +0530

    optimize message sent

--- src/api/message/create-message.js
+++ src/api/message/create-message.js
@@ -10,6 +10,7 @@ import { onlineUsers } from '../../randomConnLogic';
 import { on } from 'nodemon';
 import Joi from 'joi';
 import { validateBody } from '../../middleware/validator';
+import { createMessage } from '../../service';
 
 const validator = Joi.object({
     chatId: Joi.number().integer().required(), 
@@ -20,90 +21,72 @@ const validator = Joi.object({
 
 let controller = async (req, res, next)=>{
     const {chatId, content, identityKey} = req.body;
-    const sentBy = req.user.id;
     
     try{
-        let createdAt = new Date();
+        let createdAt = Date.now();
         io.to(chatId).emit('message', { 
             userId: req.user.id, 
             content,
             chatId,
             createdAt
         });
-        let chat = await db.Chat.findOne({
-            attributes: ['isGroupChat', 'chatName'],
-            where: {
-                id: chatId
-            }
-        });
-        if(!chat) throw new RequestError("Invalid ChatId", 400);
-        if(!chat.isGroupChat){
-            // let friend = await db.User.findOne({
-            //     attributes: ['id'],
-            //     include: [{
-            //         model: db.ChatUser,
-            //         attributes: [],
-            //         where: {
-            //             chatId
-            //         }
-            //     }],
-            //     where: {
-            //         id: {
-            //             [Op.ne]: req.user.id
-            //         }
-            //     },
-            //     raw: true
-            // });
-            // if (!friend) throw new RequestError("No friend found in this chat");
-            let [selfId, friendId] = chat.chatName.split('_');
-            if(friendId==req.user.id){
-                let temp = selfId;
-                selfId = friendId;
-                friendId = temp;
-            }
+        // let chat = await db.Chat.findOne({
+        //     attributes: ['isGroupChat', 'chatName'],
+        //     where: {
+        //         id: chatId
+        //     }
+        // });
+        // if(!chat) throw new RequestError("Invalid ChatId", 400);
+        // if(!chat.isGroupChat){
+        //     let [selfId, friendId] = chat.chatName.split('_');
+        //     if(friendId==req.user.id){
+        //         let temp = selfId;
+        //         selfId = friendId;
+        //         friendId = temp;
+        //     }
 
-            const [isBlockedByYou, isBlocked] = await Promise.all([
-                db.Friend_Request.count({ where: { from: req.user.id, to: friendId, status: "blocked" } }),
-                db.Friend_Request.count({ where: { from: friendId, to: req.user.id, status: "blocked" } }),
-            ]);
-            if (isBlockedByYou) throw new RequestError("You had blocked this user, to send the message unblock", 409);
-            if (isBlocked) throw new RequestError("The other user has blocked you");
-        }
+        //     const [isBlockedByYou, isBlocked] = await Promise.all([
+        //         db.Friend_Request.count({ where: { from: req.user.id, to: friendId, status: "blocked" } }),
+        //         db.Friend_Request.count({ where: { from: friendId, to: req.user.id, status: "blocked" } }),
+        //     ]);
+        //     if (isBlockedByYou) throw new RequestError("You had blocked this user, to send the message unblock", 409);
+        //     if (isBlocked) throw new RequestError("The other user has blocked you");
+        // }
 
-        const message = await db.Message.create({
-            chatId, content, sentBy, createdAt, updatedAt: createdAt
-        })
+        // const message = await db.Message.create({
+        //     chatId, content, sentBy: req.user.id, createdAt, updatedAt: createdAt
+        // })
         
-        db.Chat.update({
-            lastMessageId: message.id
-        }, {
-            where: {
-                id: chatId
-            }
-        }).catch(err => console.log("Error occured while updating chat from chatId: ", chatId, err));    
-        db.ChatUser.update(
-            {
-                newMessageCount: db.Sequelize.literal('newMessageCount + 1'),
-            },
-            {
-                where: {
-                    chatId,
-                    userId: {
-                        [Op.and]: [
-                            { [Op.notIn]: Object.keys(onlineUsers) }, // Exclude online users
-                            { [Op.ne]: req.user.id } // Exclude current user
-                        ]
-                    }
-                }
-            }
-        ).catch(err => console.log("Error occured while updating chatUser for chatId: ", chatId, err));    
+        // db.Chat.update({
+        //     lastMessageId: message.id
+        // }, {
+        //     where: {
+        //         id: chatId
+        //     }
+        // }).catch(err => console.log("Error occured while updating chat from chatId: ", chatId, err));    
+        // db.ChatUser.update(
+        //     {
+        //         newMessageCount: db.Sequelize.literal('newMessageCount + 1'),
+        //     },
+        //     {
+        //         where: {
+        //             chatId,
+        //             userId: {
+        //                 [Op.and]: [
+        //                     { [Op.notIn]: Object.keys(onlineUsers) }, // Exclude online users
+        //                     { [Op.ne]: req.user.id } // Exclude current user
+        //                 ]
+        //             }
+        //         }
+        //     }
+        // ).catch(err => console.log("Error occured while updating chatUser for chatId: ", chatId, err));    
+        createMessage(req, chatId, content, createdAt);
         
-        
-        res.status(201).json({
+        res.status(200).json({
             success: true,
             messages: 'Message created successfully',
             identityKey,
-            createdAt: message.createdAt,
+            createdAt,
         });
     }catch(error){
         next(error);
---
+++ src/service.js
@@ -0,0 +1,61 @@
+import db from "../models";
+
+export const createMessage = async (req, chatId, content, createdAt) => {
+    try {
+        let chat = await db.Chat.findOne({
+            attributes: ['isGroupChat', 'chatName'],
+            where: {
+                id: chatId
+            }
+        });
+        if (!chat) throw new RequestError("Invalid ChatId", 400);
+        if (!chat.isGroupChat) {
+            let [selfId, friendId] = chat.chatName.split('_');
+            if (friendId == req.user.id) {
+                let temp = selfId;
+                selfId = friendId;
+                friendId = temp;
+            }
+
+            const [isBlockedByYou, isBlocked] = await Promise.all([
+                db.Friend_Request.count({ where: { from: req.user.id, to: friendId, status: "blocked" } }),
+                db.Friend_Request.count({ where: { from: friendId, to: req.user.id, status: "blocked" } }),
+            ]);
+            if (isBlockedByYou) throw new RequestError("You had blocked this user, to send the message unblock", 409);
+            if (isBlocked) throw new RequestError("The other user has blocked you");
+        }
+
+        const message = await db.Message.create({
+            chatId, content, sentBy: req.user.id, createdAt, updatedAt: createdAt
+        })
+
+        db.Chat.update({
+            lastMessageId: message.id
+        }, {
+            where: {
+                id: chatId
+            }
+        }).catch(err => console.log("Error occured while updating chat from chatId: ", chatId, err));
+        db.ChatUser.update(
+            {
+                newMessageCount: db.Sequelize.literal('newMessageCount + 1'),
+            },
+            {
+                where: {
+                    chatId,
+                    userId: {
+                        [Op.and]: [
+                            { [Op.notIn]: Object.keys(onlineUsers) }, // Exclude online users
+                            { [Op.ne]: req.user.id } // Exclude current user
+                        ]
+                    }
+                }
+            }
+        ).catch(err => console.log("Error occured while updating chatUser for chatId: ", chatId, err));
+
+    } catch (error) {
+        console.log("Error creating the message: ", message);
+    }
+
+
+}
